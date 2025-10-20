9cdc934a3531354cb63d786ad2d77b4308d9a16f
ritik-raj-25 <ritikbunty2511@gmail.com>
2025-10-20 17:24:33 +0530

    Added particles.js background effect

--- js/script.js
+++ js/script.js
@@ -181,3 +181,79 @@ document.getElementById('resumeReader').addEventListener('click', () => {
         speakNext();
     }
 });
+
+
+// Particles.js configuration
+particlesJS('particles-js', {
+    "particles": {
+        "number": {
+            "value": 128,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#22d3ee"
+        },
+        "shape": {
+            "type": "circle",
+        },
+        "opacity": {
+            "value": 0.3,
+            "random": true,
+            "anim": {
+                "enable": true,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+                "enable": false
+            }
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#0ea5e9",
+            "opacity": 0.2,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 2,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": { enable: false, rotateX: 600, rotateY: 1200 },
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "grab"
+            },
+            "onclick": {
+                "enable": false
+            },
+            "resize": true
+        },
+        "modes": {
+            "grab": {
+                "distance": 140,
+                "line_linked": {
+                    "opacity": 0.5
+                }
+            }
+        }
+    },
+    "retina_detect": true
+});
